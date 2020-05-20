bda8058a88c5d3484e0765feacd96037dbad7827
Gabe <jflores353@gmail.com>
2020-05-20 13:40:35 -0500

    Created Counter component that has a counter with two buttons to increment and decrement the value.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import './App.css';
 
 import Toggle from './Components/Toggle';
+import Counter from './Components/Counter';
 
 function App() {
 	return (
 		<div className='App'>
 			<h2>Hooks</h2>
 			<Toggle />
+			<br />
+			<Counter />
 		</div>
 	);
 }

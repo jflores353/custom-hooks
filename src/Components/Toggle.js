f72b6ee5b3f26dd2f146ec8047b9bef6a9546746
Gabe <jflores353@gmail.com>
2020-05-20 13:01:16 -0500

    Removed useState from Toggle.js and Created useToggle() functional hook. Created toggle function and imported into Toggle.js to handle onClick for button.

--- src/Components/Toggle.js
+++ src/Components/Toggle.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useToggle } from '../hooks/useToggle';
 
 const Toggle = () => {
-	const [isToggled, setToggle] = useState(false);
+	const [isToggled, setToggle, toggle] = useToggle(false);
 
 	return (
 		<div>
-			<button onClick={() => setToggle((prevState) => !prevState)}>
-				Toggle
-			</button>
+			<button onClick={toggle}>Toggle</button>
 			{isToggled && <p>I'm totally toggled!</p>}
 		</div>
 	);
